ebbea527e8c6b2b28e133b46b02c3dbfefb2714a
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-04-08 22:02:08 -0300

    ex009 done

--- Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex009.py
+++ Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex009.py
@@ -1,1 +1,13 @@
-# 
+# Create a program that reads an integer number and displays its multiplication table on the screen (without the for loop).
+
+number = int(input('Digite um número inteiro: '))
+print(f'{number} X 1 = {number}')
+print(f'{number} X 2 = {number * 2}')
+print(f'{number} X 3 = {number * 3}')
+print(f'{number} X 4 = {number * 4}')
+print(f'{number} X 5 = {number * 5}')
+print(f'{number} X 6 = {number * 6}')
+print(f'{number} X 7 = {number * 7}')
+print(f'{number} X 8 = {number * 8}')
+print(f'{number} X 9 = {number * 9}')
+print(f'{number} X 10 = {number * 10}')
