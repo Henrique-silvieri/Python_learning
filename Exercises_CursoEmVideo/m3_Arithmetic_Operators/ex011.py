da1f33cebfdb974f7c181eaaf4925adb2b93afa4
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-04-08 22:03:50 -0300

    ex011.py

--- Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex011.py
+++ Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex011.py
@@ -1,2 +1,7 @@
-# Este � o script 11
-print('Ol�, Mundo!')
+# Create a program that reads the height and width of a wall (in meters), calculates its area, and shows how much paint will be needed to paint it, considering that each liter of paint covers 2m².
+
+height = float(input('Qual a altura da parede em metros? '))
+width = float(input('Qual a largura da parede em metros? '))
+area = height * width
+paint_needed = area / 2
+print(f'A área da parede é de {area} m², portanto a quantidade de tinta necessária de {paint_needed} L')
