d31e92789cbc25a31722f8499fad0e3593d0f401
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-04-08 22:03:11 -0300

    ex010 done

--- Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex010.py
+++ Exercises_CursoEmVideo/m3_Arithmetic_Operators/ex010.py
@@ -1,2 +1,5 @@
-# Este é o script 10
-print('Olá, Mundo!')
+# Create a program that reads the amount of money a person has in their wallet and displays how many dollars they can buy. Assume that US$1.00 = R$6.10.
+
+money = float(input('Quandos reais vocÃª tem em sua carteira? '))
+dollar = money / 6.1
+print(f'Com R${money:.2f}, vocÃª consegue comprar: US${dollar:.2f}')
