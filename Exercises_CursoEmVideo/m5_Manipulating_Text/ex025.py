c48a3848921e00c17d6e66b42a25690c67524d09
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-04-12 12:38:48 -0300

    ex025 modified

--- Exercises_CursoEmVideo/m5_Manipulating_Text/ex025.py
+++ Exercises_CursoEmVideo/m5_Manipulating_Text/ex025.py
@@ -1,5 +1,4 @@
 # Create a program that reads a person's name and says wheater or not it contains "Silva".
 
 nome = input('Digite seu nome completo: ')
-nome = nome.title()
-print('Silva' in nome)
+print (f'Seu nome tem o nome "Silva"? {'silva' in nome.lower()}')
