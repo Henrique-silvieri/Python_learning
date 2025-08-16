04108a76de80e6fa138455a0173567a39dad8ab9
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-08-16 14:19:01 -0300

    ex058 done

---
+++ Exercises_CursoEmVideo/Wold_2/m9_While_Loop_Structure/ex058.py
@@ -0,0 +1,18 @@
+# Improve the game from ex028, where the computer will "think" of a number between 0 and 10.
+# Now, however, the player will try to guess until they get it right, and at the end, the program should display how many attempts were needed to win.
+
+from random import randint
+
+guess = int(input('Pensei em um número de 1 a 10, tente adivinhar: '))
+number = randint(1, 10)
+attemps = 0
+
+if guess == number:
+    print('UAU! Parebéns, você acertou de primeira!')
+else: 
+    while guess != number:
+        guess = int(input('Você errou! Digite um número de 1 a 10: '))
+        attemps+=1
+        number = randint(1, 10)
+    
+    print(f'Parabéns!!! Você acertou! houve um total de {attemps+1} tentativas.')
