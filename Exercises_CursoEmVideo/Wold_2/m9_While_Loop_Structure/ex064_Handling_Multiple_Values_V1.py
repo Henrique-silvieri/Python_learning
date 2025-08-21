f21db6f6e2c52b7fc39e5b68ca223e3621cf4c00
Henrique Silviéri <silvieri.henrique@gmail.com>
2025-08-20 21:46:00 -0300

    Adjust ex064

--- Exercises_CursoEmVideo/Wold_2/m9_While_Loop_Structure/ex064_Handling_Multiple_Values_V1.py
+++ Exercises_CursoEmVideo/Wold_2/m9_While_Loop_Structure/ex064_Handling_Multiple_Values_V1.py
@@ -1,15 +1,17 @@
 # Create a program that reads multiple integer numbers from the keyboard. The program should only stop when the user enters the value 999, which is the stop condition. At the end, display how many numbers were entered and what their sum was (disregarding the flag).
 
 flag = 999
-sum = 0
+total = 0
 number = 0
+count = 0
 
 print('-Programa de soma-\n')
 while number != flag:
-    number = int(input('Digite um número para adiionar a soma (ou digite "999" para sair): '))
+    number = int(input('Digite um número para adicionar a soma (ou digite "999" para sair): '))
     if number != flag:
-        sum += number
+        total += number
     else:
         break
+    count += 1
 
-print(f'O valor da soma dos números digitados é {sum}.')
+print(f'Foram digitados {count} números, e o valor da soma dos números digitados é {total}.')
